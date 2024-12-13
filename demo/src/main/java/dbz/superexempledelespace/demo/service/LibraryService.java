b0a154feae9a64ca8548cf948a5f89b05f7acfda
Nassim Sakhri <tails01@hotmail.fr>
2024-12-13 16:12:53 +0100

    Ajout exo AOP

--- demo/src/main/java/dbz/superexempledelespace/demo/service/LibraryService.java
+++ demo/src/main/java/dbz/superexempledelespace/demo/service/LibraryService.java
@@ -1,5 +1,9 @@
 package dbz.superexempledelespace.demo.service;
 
+import dbz.superexempledelespace.demo.annotation.AfterAnnotation;
+import dbz.superexempledelespace.demo.annotation.AroundAnnotation;
+import dbz.superexempledelespace.demo.annotation.BeforeAnnotation;
+import dbz.superexempledelespace.demo.annotation.ExecptionAnnotation;
 import dbz.superexempledelespace.demo.entity.Book;
 import org.springframework.stereotype.Service;
 
@@ -10,25 +14,39 @@ import java.util.List;
 
 @Service
 public class LibraryService {
-List<Book> bouquins = new ArrayList<Book>();
-    public Book makeBook(String titre,LocalDate datePublication,String auteur, String categorie, double prix,boolean prixNobel){
-        Book book = new Book(titre,datePublication,auteur,categorie,prix,prixNobel);
-        bouquins.add(book);
-        return book;
+
+    List<Book> bouquins = new ArrayList<>();
+
+
+
+    @BeforeAnnotation
+    @AroundAnnotation
+    @AfterAnnotation
+    public Book makeBook(String titre, LocalDate datePublication, String auteur, String categorie, double prix, boolean prixNobel){
+        Book livre = new Book(titre,datePublication,auteur,categorie,prix,prixNobel);
+        bouquins.add(livre);
+        System.out.println(bouquins);
+        return livre;
     }
 
+
+    @BeforeAnnotation
+    @AroundAnnotation
+    @AfterAnnotation
     public void deleteBook(String titre){
-        for (Book book : bouquins) {
-            if (book.getTitre().equals(titre)) {
-                bouquins.remove(book);
-            }
-        }
+        bouquins.removeIf(book -> book.getTitre().equals(titre));
     }
 
+    @BeforeAnnotation
+    @AroundAnnotation
+    @AfterAnnotation
     public List<Book> getBooks(){
-        return bouquins;
+        return this.bouquins;
     }
 
-
+    @ExecptionAnnotation
+    public void methodThrow(int arg,String arg2){
+        throw new IllegalArgumentException("execption de la superbe et incroyable méthode");
+    }
 
 }
