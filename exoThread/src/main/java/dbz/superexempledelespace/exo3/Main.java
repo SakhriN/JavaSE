c01eae3c135b1f9b3836ba4f673a054a8fbd6241
Nassim Sakhri <tails01@hotmail.fr>
2024-12-16 16:10:37 +0100

    Ajout 3 nouveaux exercices

--- exoThread/src/main/java/dbz/superexempledelespace/exo3/Main.java
+++ exoThread/src/main/java/dbz/superexempledelespace/exo3/Main.java
@@ -3,21 +3,24 @@ package dbz.superexempledelespace.exo3;
 import java.util.concurrent.*;
 
 public class Main {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+    public static void main(String[] args) {
+        try {
+            ExecutorService executor = Executors.newSingleThreadExecutor();
+            Callable<String> callableTask = () -> {
+                for (int i = 1; i <= 5; i++) {
+                    CalculatorCallable calculatorCallable = new CalculatorCallable(i);
+                    System.out.println("Le cube de " + i + " : " + calculatorCallable.call());
+                    Thread.sleep(400);
+                }
+                executor.shutdown();
+                return "Resultat du callable terminé";
+            };
+            Future<String> futureResult = executor.submit(callableTask);
 
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        Callable<String> callableTask = () -> {
-            for (int i = 1; i <= 5;i++){
-                CalculatorCallable calculatorCallable = new CalculatorCallable(i);
-                System.out.println("Le cube de " + i + " : " + calculatorCallable.call());
-                Thread.sleep(400);
-            }
-            return "Resultat du callable terminé";
-        };
-
-        Future<String> futureResult = executor.submit(callableTask);
-
-        System.out.println("Resultat de callable : "+futureResult.get());
-        executor.shutdown();
+            System.out.println("Resultat de callable : " + futureResult.get());
+            executor.shutdown();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 }
