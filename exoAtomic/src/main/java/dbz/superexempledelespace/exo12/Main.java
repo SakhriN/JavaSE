04a5c6d3b8d98fe26e54f6ac442cadf0c576bbfd
Nassim Sakhri <tails01@hotmail.fr>
2024-12-18 09:45:16 +0100

    Ajout exo 12

--- exoAtomic/src/main/java/dbz/superexempledelespace/exo12/Main.java
+++ exoAtomic/src/main/java/dbz/superexempledelespace/exo12/Main.java
@@ -5,9 +5,31 @@ public class Main {
 
     public static void main(String[] args) {
 
-        
+        SharedResource sharedResource = new SharedResource();
 
+        Thread[] threads = new Thread[2];
+        for (int i = 0; i < threads.length; i++) {
+            int finalI = i;
+            threads[i] = new Thread(()->{
+                switch(finalI){
+                    case 0->{
+                        for (int j = 0; j < 5; j++) {
+                            int finalJ = j;
+                            sharedResource.remove(finalJ);
+                        }
+                    }
+                    default -> {
+                        for (int j = 0; j < 5; j++) {
+                            int finalJ = j;
+                            sharedResource.add(finalJ);
+                        }
+                    }
+                }
+            });
+        }
 
-
+        for (int i = 0; i < threads.length; i++) {
+            threads[i].start();
+        }
     }
 }
--- exoAtomic/src/main/java/dbz/superexempledelespace/exo12/SharedResource.java
+++ exoAtomic/src/main/java/dbz/superexempledelespace/exo12/SharedResource.java
@@ -2,6 +2,7 @@ package dbz.superexempledelespace.exo12;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -13,25 +14,39 @@ public class SharedResource {
         list = new ArrayList<Integer>();
     }
 
-    public void add(Integer value) {
-        boolean bool = lock.tryLock();
-        if (bool) {
-            lock.lock();
-            list.add(value);
-            lock.unlock();
-        }else{
-            System.out.println("Raté");
+    public void add(Integer value)  {
+        try{
+            boolean bool = lock.tryLock(4, TimeUnit.SECONDS);
+            if (bool) {
+                list.add(value);
+                System.out.println(Thread.currentThread().getName() + " a ajouté " + value);
+                lock.unlock();
+
+            }else{
+                System.out.println("Raté");
+            }
+        }catch(InterruptedException e){
+            e.printStackTrace();
         }
+
     }
 
     public void remove(Integer value) {
-        boolean bool = lock.tryLock();
-        if (bool) {
-            lock.lock();
-            list.remove(value);
-            lock.unlock();
-        }else{
-            System.out.println("Raté");
+        try{
+            boolean bool = lock.tryLock(4,TimeUnit.SECONDS);
+            if (bool) {
+                if(list.contains(value)){
+                    list.remove(value);
+                    System.out.println(Thread.currentThread().getName() + " a supprimé " + value);
+                }else{
+                    System.out.println(Thread.currentThread().getName() + " n'a rien à supprimer.");
+                }
+                lock.unlock();
+            }else{
+                System.out.println("Raté");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
