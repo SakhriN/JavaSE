f3624fe346e5862cb4bb908bd728d0d6c43537ed
Nassim Sakhri <tails01@hotmail.Fr>
2025-01-06 15:22:27 +0100

    Fin des vacances, envoi exercice Modularité multi modules

---
+++ exoModulaireRapport/EmailNotification/src/module-info.java
@@ -0,0 +1,7 @@
+
+
+module EmailNotification {
+    requires NotificationService;
+    provides notification.NotificationService with eNotification.EmailNotification;
+    exports eNotification to LibraryApp;
+}
